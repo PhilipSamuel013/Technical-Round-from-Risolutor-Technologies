c9d3f4d77c03897d5a712b00ca162f55d3cce03b
bala suriya <bala.suriyas@cognizant.com>
2025-01-25 15:27:44 +0530

    Initial commit

---
+++ Find the First Unique Number in an Array/FindUniqueNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 4, 10, 5, 4, 2, 10 };
+
+        int result = FindFirstUniqueNumber(array);
+
+        if (result != -1)
+        {
+            Console.WriteLine($"The first unique number {result}");
+        }
+        else
+        {
+            Console.WriteLine("No unique number found.");
+        }
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="array"></param>
+    /// <returns></returns>
+    
+    static int FindFirstUniqueNumber(int[] array)
+    {
+        var unique = array.GroupBy(x => x)           
+                          .Where(g => g.Count() == 1) 
+                          .Select(g => g.Key)         
+                          .FirstOrDefault();          
+
+        return unique == 0 ? -1 : unique; 
+    }
+}
---
+++ Find the Index of the String/StringIndex/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string S = "racecar";
+        int result = Solution(S);
+        Console.WriteLine(result);
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <param name="S"></param>
+    /// <returns></returns>
+
+    static int Solution(string S)
+    {
+        int n = S.Length;
+        
+        for (int i = 0; i < n; i++)
+        {
+            string leftPart = S.Substring(0, i);    // Left of the character
+            string rightPart = S.Substring(i + 1);  // Right of the character
+
+            // Compare the left part with the reversed right part
+            if (leftPart == string.Join("", rightPart.ToCharArray().Reverse()))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
